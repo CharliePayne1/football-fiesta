5f3c98730848587d8b3c29fe4fc2f9f3c2fb0dff
Charlie Payne <charliepayne1101@gmail.com>
2021-10-13 18:55:02 +0100

    create teams picker and previously chosen teams

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Teams from './components/Teams/Teams'
+import Scores from './components/Scores/Scores'
+
+const teamsFromAPI = ['Arsenal', 'Aston Villa', 'Brentford', 'Brighton and Hove Albion', 'Burnley', 'Chelsea', 'Crystal Palace', 'Everton', 'Leeds United', 'Leicester City', 'Liverpool', 'Manchester City', 'Manchester United', 'Newcastle United', 'Norwich City', 'Southampton', 'Tottenham Hotspur', 'Watford', 'West Ham United'];
+const teamsChosenFromAPI = ['Arsenal'];
 
 function App() {
+  const [premierTeams, setPremierTeams] = useState([]);
+  const [pickedTeams, setPickedTeams] = useState([]);
+
+  useEffect(() => {
+    setPremierTeams(teamsFromAPI);
+    setPickedTeams(teamsChosenFromAPI)
+  });
+
+  const pickTeam = (pos) => {
+    premierTeams = premierTeams.filter(teams => teams[pos] !== pos)
+  }
+  
   return (
-    <>
-    Football fiesta
-    </>
+    <div className="App">
+    <p className="title">
+      <span>
+        Last Man Standing
+      </span>
+    </p>
+    <Teams teams={premierTeams} pickTeam={pickTeam}/>
+    <Scores teams={pickedTeams}/>
+    </div>
   );
 }
 
